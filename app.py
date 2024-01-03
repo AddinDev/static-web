fddddf3b732d8b81c2dba900c8940b50d28a2189
addin <addinsatria2004@gmail.com>
2024-01-03 18:04:04 +0700

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('static.html')
+    return render_template('public/static.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
